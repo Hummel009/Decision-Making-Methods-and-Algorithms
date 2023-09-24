40e2782e20b31972c052aec1e0e9ceb142894f7b
Hummel009 <hummel.turbamentum@gmail.com>
2023-09-24 12:04:12 +0300

    unneeded, comms

--- Lab04/src/main/kotlin/hummel/Main.kt
+++ Lab04/src/main/kotlin/hummel/Main.kt
@@ -2,6 +2,8 @@ package hummel
 
 fun main() {
 	println("Reading images...")
+
+	// Создание списков изображений для каждой категории: S, N, T.
 	val s1Images = ((0..3).map { PictureImage("new/0$it.jpg", 1) })
 	val n1Images = ((0..3).map { PictureImage("new/1$it.jpg", 0) })
 	val t1Images = ((0..3).map { PictureImage("new/2$it.jpg", 0) })
@@ -12,19 +14,29 @@ fun main() {
 	val n3Images = ((0..3).map { PictureImage("new/1$it.jpg", 0) })
 	val t3Images = ((0..3).map { PictureImage("new/2$it.jpg", 1) })
 
+	// Объединение списков изображений для каждой категории.
 	val sImages = s1Images + n1Images + t1Images
 	val nImages = s2Images + n2Images + t2Images
 	val tImages = s3Images + n3Images + t3Images
 
+	// Создание трех персептронов для каждой категории.
 	val perceptron1 = Perceptron(imagePixelCount = 36, iterationCount = 10000000)
 	val perceptron2 = Perceptron(imagePixelCount = 36, iterationCount = 10000000)
 	val perceptron3 = Perceptron(imagePixelCount = 36, iterationCount = 10000000)
+
 	println("Perceptron 1 training...")
+	// Обучение первого персептрона на изображениях из категории S.
 	perceptron1.train(sImages)
+
 	println("Perceptron 2 training...")
+	// Обучение второго персептрона на изображениях из категории N.
 	perceptron2.train(nImages)
+
 	println("Perceptron 3 training...")
+	// Обучение третьего персептрона на изображениях из категории T.
 	perceptron3.train(tImages)
+
+	// Проверка наличия целевых изображений с помощью обученных персептронов.
 	println("Is positive test passed (S): ${perceptron1.isTargetImage(PictureImage("new/s-test.jpg", 1))}")
 	println("Is positive test passed (N): ${perceptron2.isTargetImage(PictureImage("new/n-test.jpg", 1))}")
 	println("Is positive test passed (T): ${perceptron3.isTargetImage(PictureImage("new/t-test.jpg", 1))}")
--- Lab04/src/main/kotlin/hummel/Perceptron.kt
+++ Lab04/src/main/kotlin/hummel/Perceptron.kt
@@ -8,22 +8,30 @@ fun imageToPixelArray(fileName: String): IntArray {
 	val classLoader = Perceptron::class.java.classLoader
 	val file = File(classLoader.getResource(fileName)!!.file)
 	val image = ImageIO.read(file)
+	// Создаем массив для хранения пикселей (0 - белый, 1 - черный)
 	val pixels = IntArray(image.width * image.height)
+	// Проходим по всем пикселям изображения
 	for (x in 0 until image.width) {
 		for (y in 0 until image.height) {
+			// Преобразуем цвет пикселя в 0 или 1 и сохраняем в массиве
 			pixels[x * image.width + y] = (if (image.getRGB(x, y) == 0xFFFFFFFF.toInt()) 0 else 1)
 		}
 	}
+	// Возвращаем массив пикселей
 	return pixels
 }
 
+// Определяем класс для хранения изображения и метки
 data class PictureImage(val pixelArray: IntArray, var isTargetImage: Int) {
 	constructor(fileName: String, targetImageProbability: Int) : this(
+		// Создаем объект PictureImage, используя функцию imageToPixelArray
 		imageToPixelArray(fileName), targetImageProbability
 	)
 
+	// Метод для вычисления суммы взвешенных значений пикселей
 	fun calculateWeightSum(weights: IntArray): Int {
 		var sum = 0
+		// Проходим по всем пикселям и умножаем их на соответствующие веса
 		weights.indices.forEach { i ->
 			sum += weights[i] * pixelArray[i]
 		}
@@ -31,13 +39,21 @@ data class PictureImage(val pixelArray: IntArray, var isTargetImage: Int) {
 	}
 
 	override fun equals(other: Any?): Boolean {
-		if (this === other) return true
-		if (javaClass != other?.javaClass) return false
+		if (this === other) {
+			return true
+		}
+		if (javaClass != other?.javaClass) {
+			return false
+		}
 
 		other as PictureImage
 
-		if (!pixelArray.contentEquals(other.pixelArray)) return false
-		if (isTargetImage != other.isTargetImage) return false
+		if (!pixelArray.contentEquals(other.pixelArray)) {
+			return false
+		}
+		if (isTargetImage != other.isTargetImage) {
+			return false
+		}
 
 		return true
 	}
@@ -49,24 +65,34 @@ data class PictureImage(val pixelArray: IntArray, var isTargetImage: Int) {
 	}
 }
 
+// Определяем класс Perceptron для обучения
 class Perceptron(
 	imagePixelCount: Int, private val threshold: Double = 0.0, val iterationCount: Int = 10000
 ) {
+	// Массив весов
 	private val weights: IntArray = IntArray(imagePixelCount)
 
+	// Метод для обучения персептрона на заданном наборе обучающих изображений.
 	fun train(trainingSet: List<PictureImage>) {
 		var prevPercent = -1
+		// Проходим через заданное количество итераций обучения.
 		for (it in 1..iterationCount) {
+			// Случайным образом выбираем изображение из обучающего набора.
 			trainingSet.randomItem { image ->
+				// Вычисляем выход персептрона для выбранного изображения.
 				val output = calculateOutput(image)
+				// Проверяем, совпадает ли выход с желаемой меткой (целевым изображением).
 				if (image.isTargetImage != output) {
 					if (image.isTargetImage == 0) {
+						// Если выход неверный и изображение не является целевым, наказываем персептрон.
 						punish(image)
 					} else {
+						// Если выход неверный и изображение является целевым, стимулируем персептрон.
 						stimulate(image)
 					}
 				}
 			}
+			// Вычисляем процент завершенности обучения и выводим информацию о процессе каждые 10%.
 			val percent = (it.toDouble() / iterationCount * 100).toInt()
 			if ((percent % 10 == 0) && prevPercent != percent) {
 				prevPercent = percent
@@ -75,7 +101,9 @@ class Perceptron(
 		}
 	}
 
+	// Метод для наказания персептрона в случае неверного выхода.
 	private fun punish(image: PictureImage) {
+		// Проходим по всем пикселям изображения и уменьшаем соответствующие веса, если пиксель активен (1).
 		weights.indices.forEach { i ->
 			if (image.pixelArray[i] == 1) {
 				weights[i] -= 1
@@ -83,7 +111,9 @@ class Perceptron(
 		}
 	}
 
+	// Метод для стимулирования персептрона в случае неверного выхода.
 	private fun stimulate(image: PictureImage) {
+		// Проходим по всем пикселям изображения и увеличиваем соответствующие веса, если пиксель активен (1).
 		weights.indices.forEach { i ->
 			if (image.pixelArray[i] == 1) {
 				weights[i] += 1
@@ -91,11 +121,15 @@ class Perceptron(
 		}
 	}
 
+	// Метод для вычисления выхода персептрона
 	private fun calculateOutput(image: PictureImage) = if (image.calculateWeightSum(weights) >= threshold) 1 else 0
 
+	// Метод для проверки, является ли изображение целевым
 	fun isTargetImage(image: PictureImage): Boolean = calculateOutput(image) == 1
 
 	private val random = Random()
+
+	// Расширение списка для выбора случайного элемента и выполнения действия над ним
 	private fun <E> List<E>.randomItem(action: (item: E) -> Unit) {
 		action(this[random.nextInt(this.size)])
 	}
