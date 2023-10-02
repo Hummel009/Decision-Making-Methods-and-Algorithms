723900d03e10ef9f03be0241e110e8103a71c817
Hummel009 <hummel.turbamentum@gmail.com>
2023-10-02 21:35:20 +0300

    1

--- Lab05/src/main/kotlin/hummel/Main.kt
+++ Lab05/src/main/kotlin/hummel/Main.kt
@@ -2,11 +2,16 @@ package hummel
 
 import java.util.*
 
+// Инициализация объекта для генерации случайных чисел
 val RANDOM: Random = Random()
 
+// Класс, представляющий диапазон значений
 data class Range(val from: Double, val to: Double) {
+
+	// Генерация случайного числа в пределах диапазона
 	fun randomInto(): Double = from + (to - from) * RANDOM.nextDouble()
 
+	// Выполнение действия для каждого значения с шагом
 	fun forEachWithStep(step: Double, action: (currentValue: Double) -> Unit) {
 		var current = from
 		while (current < to) {
@@ -16,28 +21,42 @@ data class Range(val from: Double, val to: Double) {
 	}
 }
 
+// Задание диапазонов для координат X и Y
 val X_RANGE: Range = Range(-5.0, 5.0)
 val Y_RANGE: Range = Range(-3.0, 3.0)
+
+// Задание обучающего набора по умолчанию
 val TRAIN_SET_DEFAULT: Array<Point> = arrayOf(
 	-1.0 to 0.0 classifiedBy 1, 1.0 to 1.0 classifiedBy 1, 2.0 to 0.0 classifiedBy 2, 1.0 to -2.0 classifiedBy 2
 )
 
+// Задание обучающего набора для пересекающихся линий
 val TRAIN_DATA_INTERSECTED_LINES: Array<Point> = arrayOf(
 	-1.0 to -1.0 classifiedBy 1, 1.0 to 1.0 classifiedBy 1, -1.0 to 1.0 classifiedBy 2, 1.0 to -1.0 classifiedBy 2
 )
 
+// Количество случайных точек для генерации
 const val POINT_NUMBER: Int = 1000
 
+// Основная функция
 fun main() {
+	// Массив обучающих наборов
 	val dataSets = arrayOf(TRAIN_SET_DEFAULT, TRAIN_DATA_INTERSECTED_LINES)
+
+	// Итерация по обучающим наборам
 	dataSets.forEachIndexed { index, dataSet ->
+		// Генерация функции принятия решений и функции для графика
 		val (decisionFunction, chartFunction) = generateDecisionFunction(dataSet)
+
+		// Генерация случайных неклассифицированных точек
 		val points = mutableListOf<UnclassifiedPoint>()
 		for (i in 1..POINT_NUMBER) {
 			points.add(
 				X_RANGE.randomInto() to Y_RANGE.randomInto()
 			)
 		}
+
+		// Классификация сгенерированных точек и отрисовка графика
 		val classifiedPoints = points.map { decisionFunction(it) }
 		draw(classifiedPoints, X_RANGE, Y_RANGE, chartFunction, index)
 	}
--- Lab05/src/main/kotlin/hummel/PotentialClassifier.kt
+++ Lab05/src/main/kotlin/hummel/PotentialClassifier.kt
@@ -1,45 +1,62 @@
 package hummel
 
+// Создание точки из двух координат
 infix fun Double.to(y: Double): UnclassifiedPoint = UnclassifiedPoint(this, y)
+
+// Присвоение класса точке
 infix fun UnclassifiedPoint.classifiedBy(classIndex: Int): Point = Point(this.x, this.y, classIndex)
 
+// Класс представляющий точку без классификации
 data class UnclassifiedPoint(val x: Double, val y: Double)
 
+// Класс представляющий точку с классификацией
 data class Point(val x: Double, val y: Double, val classIndex: Int)
+
+// Структура данных, содержащая функции для решений и построения графика
 data class GeneratedFunctions(val decisionFunction: PotentialDecisionFunction, val chartFunction: ChartFunction)
 
+// Коэффициенты для потенциальной функции
 private val ERMIT_COEFFICIENTS = PotentialCoefficients(1.0, 4.0, 4.0, 16.0)
 private val INITIAL_COEFFICIENTS = PotentialCoefficients(0.0, 0.0, 0.0, 0.0)
 
+// Типы для удобства использования
 typealias PotentialDecisionFunction = (UnclassifiedPoint) -> Point
 typealias ChartFunction = (Double) -> Double
 
+// Тип для представления потенциальной функции
 private typealias PotentialFunction = (Point) -> Double
 
+// Класс, представляющий коэффициенты потенциальной функции
 data class PotentialCoefficients(val first: Double, val second: Double, val third: Double, val fourth: Double) {
 
+	// Перегрузка операторов для удобства работы с коэффициентами
 	operator fun plus(other: PotentialCoefficients): PotentialCoefficients =
 		PotentialCoefficients(first + other.first, second + other.second, third + other.third, fourth + other.fourth)
 
 	operator fun times(c: Double): PotentialCoefficients =
 		PotentialCoefficients(c * first, c * second, c * third, c * fourth)
 
+	// Подстановка значений точки в коэффициенты
 	fun substitutePoint(point: Point): PotentialCoefficients =
 		PotentialCoefficients(first, second * point.x, third * point.y, fourth * point.x * point.y)
 
+	// Получение потенциальной функции
 	fun getPotentialFunction(): PotentialFunction = { (x, y, _) ->
 		first + second * x + third * y + fourth * x * y
 	}
 
+	// Получение функции для построения графика
 	fun getChartFunction(): (Double) -> Double = {
 		-(first + second * it) / (third + fourth * it)
 	}
 
+	// Строковое представление коэффициентов
 	override fun toString(): String {
 		return "$first + ($second)*X + ($third)*Y + ($fourth)*X*Y"
 	}
 }
 
+// Функция для получения коэффициента нормализации
 private fun getNormalizationCoefficient(potentialValue: Double, classIndex: Int) =
 	if (classIndex == 1 && potentialValue <= 0) {
 		1.0
@@ -49,6 +66,7 @@ private fun getNormalizationCoefficient(potentialValue: Double, classIndex: Int)
 		0.0
 	}
 
+// Генерация функции принятия решений и функции для графика
 fun generateDecisionFunction(
 	trainSet: Array<Point>, initialPotentialCoefficients: PotentialCoefficients = INITIAL_COEFFICIENTS
 ): GeneratedFunctions = generateDecisionFunction(trainSet.asList(), initialPotentialCoefficients)
