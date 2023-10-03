a2abd611f4a0e7c75283779b3dbf2f4cd3a6a08e
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-10-03 22:43:59 +0300

    1

--- Lab05/src/main/kotlin/hummel/Draw.kt
+++ Lab05/src/main/kotlin/hummel/Draw.kt
@@ -10,46 +10,29 @@ import org.jfree.data.xy.XYSeriesCollection
 import java.awt.BasicStroke
 import java.io.File
 
-// Функция для отрисовки графика распознавания объектов методом потенциалов
-fun draw(points: List<Point>, xRange: Range, yRange: Range, chartFunction: ChartFunction, index: Int) {
-	// Создание коллекции данных для графика
+fun draw(points: List<ClassifiedPoint>, xRange: Range, yRange: Range, chartFunction: ChartFunction, index: Int) {
 	val dataset = XYSeriesCollection()
 
-	// Создание серий данных для классов и функции разделения
 	val series1 = XYSeries("Class 1 points")
 	val series2 = XYSeries("Class 2 points")
 	val series3 = XYSeries("Separation function")
 
-	// Заполнение серий данными точек классов
-	points.forEach { (x, y, classIndex) ->
-		if (classIndex == 1) {
-			series1
-		} else {
-			series2
-		}.add(x, y)
-	}
+	points.forEach { (x, y, classIndex) -> (if (classIndex == 1) series1 else series2).add(x, y) }
 
-	// Заполнение серии данных для функции разделения
-	xRange.forEachWithStep(0.01) {
-		series3.add(it, chartFunction(it))
-	}
+	xRange.forEachWithStep(0.01) { series3.add(it, chartFunction(it)) }
 
-	// Добавление серий в коллекцию данных
 	dataset.addSeries(series1)
 	dataset.addSeries(series2)
 	dataset.addSeries(series3)
 
-	// Создание графика с использованием библиотеки JFreeChart
 	val chart = ChartFactory.createXYLineChart(
 		"Potential Classifier", "X", "Y", dataset, PlotOrientation.VERTICAL, true, false, false
 	)
 
-	// Получение объекта XYPlot для настройки осей
-	val plot: XYPlot = chart.plot as XYPlot
+	val plot = chart.plot as XYPlot
 	plot.domainAxis.setRange(xRange.from, xRange.to)
 	plot.rangeAxis.setRange(yRange.from, yRange.to)
 
-	// Получение объекта рендерера для настройки внешнего вида графика
 	val renderer = plot.renderer as XYLineAndShapeRenderer
 	renderer.setSeriesLinesVisible(0, false)
 	renderer.setSeriesShapesVisible(0, true)
@@ -57,6 +40,5 @@ fun draw(points: List<Point>, xRange: Range, yRange: Range, chartFunction: Chart
 	renderer.setSeriesShapesVisible(1, true)
 	renderer.setSeriesStroke(2, BasicStroke(2F))
 
-	// Сохранение графика в файл формата PNG
 	ChartUtils.saveChartAsPNG(File("images/classification-chart-$index.jpg"), chart, 1000, 1000)
 }
--- Lab05/src/main/kotlin/hummel/Main.kt
+++ Lab05/src/main/kotlin/hummel/Main.kt
@@ -2,62 +2,46 @@ package hummel
 
 import java.util.*
 
-// Инициализация объекта для генерации случайных чисел
-val RANDOM: Random = Random()
+val random: Random = Random()
+val xRange: Range = Range(-5.0, 5.0)
+val yRange: Range = Range(-3.0, 3.0)
 
-// Класс, представляющий диапазон значений
+val trainSet: MutableList<ClassifiedPoint> = mutableListOf(
+	ClassifiedPoint(-1.0, 0.0, 1),
+	ClassifiedPoint(1.0, 1.0, 1),
+	ClassifiedPoint(2.0, 0.0, 2),
+	ClassifiedPoint(1.0, -2.0, 2)
+)
+
+val trainSetIntersectedLines: MutableList<ClassifiedPoint> = mutableListOf(
+	ClassifiedPoint(-1.0, -1.0, 1),
+	ClassifiedPoint(1.0, 1.0, 1),
+	ClassifiedPoint(-1.0, 1.0, 2),
+	ClassifiedPoint(1.0, -1.0, 2)
+)
+
+fun main() {
+	val dataSets = arrayOf(trainSet, trainSetIntersectedLines)
+
+	dataSets.forEachIndexed { index, dataSet ->
+		//разделяющая функция и решающее правило
+		val (decisionFunction, chartFunction) = generateDecisionFunction(dataSet)
+
+		val points = List(1000) { UnclassifiedPoint(xRange.random(), yRange.random()) }
+
+		val classifiedPoints = points.map { decisionFunction(it) }
+		draw(classifiedPoints, xRange, yRange, chartFunction, index)
+	}
+}
+
 data class Range(val from: Double, val to: Double) {
+	fun random(): Double = from + (to - from) * random.nextDouble()
 
-	// Генерация случайного числа в пределах диапазона
-	fun randomInto(): Double = from + (to - from) * RANDOM.nextDouble()
-
-	// Выполнение действия для каждого значения с шагом
-	fun forEachWithStep(step: Double, action: (currentValue: Double) -> Unit) {
+	fun forEachWithStep(step: Double, action: (Double) -> Unit) {
 		var current = from
 		while (current < to) {
 			action(current)
 			current += step
 		}
-	}
-}
-
-// Задание диапазонов для координат X и Y
-val X_RANGE: Range = Range(-5.0, 5.0)
-val Y_RANGE: Range = Range(-3.0, 3.0)
-
-// Задание обучающего набора по умолчанию
-val TRAIN_SET_DEFAULT: Array<Point> = arrayOf(
-	-1.0 to 0.0 classifiedBy 1, 1.0 to 1.0 classifiedBy 1, 2.0 to 0.0 classifiedBy 2, 1.0 to -2.0 classifiedBy 2
-)
-
-// Задание обучающего набора для пересекающихся линий
-val TRAIN_DATA_INTERSECTED_LINES: Array<Point> = arrayOf(
-	-1.0 to -1.0 classifiedBy 1, 1.0 to 1.0 classifiedBy 1, -1.0 to 1.0 classifiedBy 2, 1.0 to -1.0 classifiedBy 2
-)
-
-// Количество случайных точек для генерации
-const val POINT_NUMBER: Int = 1000
-
-// Основная функция
-fun main() {
-	// Массив обучающих наборов
-	val dataSets = arrayOf(TRAIN_SET_DEFAULT, TRAIN_DATA_INTERSECTED_LINES)
-
-	// Итерация по обучающим наборам
-	dataSets.forEachIndexed { index, dataSet ->
-		// Генерация функции принятия решений и функции для графика
-		val (decisionFunction, chartFunction) = generateDecisionFunction(dataSet)
-
-		// Генерация случайных неклассифицированных точек
-		val points = mutableListOf<UnclassifiedPoint>()
-		for (i in 1..POINT_NUMBER) {
-			points.add(
-				X_RANGE.randomInto() to Y_RANGE.randomInto()
-			)
-		}
-
-		// Классификация сгенерированных точек и отрисовка графика
-		val classifiedPoints = points.map { decisionFunction(it) }
-		draw(classifiedPoints, X_RANGE, Y_RANGE, chartFunction, index)
 	}
 }
