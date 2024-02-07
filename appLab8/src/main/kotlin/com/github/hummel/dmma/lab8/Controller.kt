039e35f3061fd62c38291ace9757df74c44318cb
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-07 18:04:22 +0300

    Update Controller.kt

--- appLab8/src/main/kotlin/com/github/hummel/dmma/lab8/Controller.kt
+++ appLab8/src/main/kotlin/com/github/hummel/dmma/lab8/Controller.kt
@@ -5,7 +5,6 @@ import javafx.fxml.FXML
 import javafx.scene.canvas.Canvas
 import javafx.scene.control.Label
 import javafx.scene.input.MouseEvent
-import lab8.grammar.*
 
 @Suppress("unused", "MemberVisibilityCanBePrivate")
 class Controller {
