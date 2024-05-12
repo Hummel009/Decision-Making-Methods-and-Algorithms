664631f3ac2bb0a4fa9d5b596fad69e6658b6e81
Hummel009 <hummel.turbamentum@gmail.com>
2024-05-12 15:46:40 +0300

    Kotlin 1.9.24

--- appLab6/build.gradle.kts
+++ appLab6/build.gradle.kts
@@ -12,7 +12,7 @@ version = LocalDate.now().format(DateTimeFormatter.ofPattern("yy.MM.dd"))
 val embed: Configuration by configurations.creating
 
 dependencies {
-	embed("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
+	embed("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
 	embed("io.github.pityka:hierarchical-clustering-fork:1.0-5")
 	implementation("io.github.pityka:hierarchical-clustering-fork:1.0-5")
 }
